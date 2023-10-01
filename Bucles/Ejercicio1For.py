72856b5fb67cc23d78253050aec9372dd8ee18a9
Rony López <49849572+RonyLopez@users.noreply.github.com>
2023-10-01 17:21:12 -0600

    Update Ejercicio1For.py

--- Bucles/Ejercicio1For.py
+++ Bucles/Ejercicio1For.py
@@ -1,6 +1,18 @@
 '''Ejercicio 1
 
 Imprimir por pantalla los numeros del 1 al 10, luego, 
-çpedir al usuario dos numeros y mostrar el rango de numeros 
+pedir al usuario dos numeros y mostrar el rango de numeros 
 entre ambas cifras '''
 
+for i in range(0,10):
+    print(i+1)
+valor1 = int(input("Ingrese el valor inicial: "))
+valor2 = int(input("Ingrese el valor final: "))
+
+if valor1 <0 or valor2 >10:
+    print("Los valores proporcionados son incorrecto, intente de nuevo")
+else:
+    for i in range(valor1-1,valor2):
+        print(i+1)
+
+             
