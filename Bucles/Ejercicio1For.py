85452d46dfa250848725160eac79b5b92429a7d8
Rony López <49849572+RonyLopez@users.noreply.github.com>
2023-10-01 16:37:00 -0600

    Ejercicios adicionales

---
+++ Bucles/Ejercicio1For.py
@@ -0,0 +1,6 @@
+'''Ejercicio 1
+
+Imprimir por pantalla los numeros del 1 al 10, luego, 
+çpedir al usuario dos numeros y mostrar el rango de numeros 
+entre ambas cifras '''
+
---
+++ Bucles/ejercicio2.py
@@ -0,0 +1,10 @@
+edad = int(input("Ingrese la edad: "))
+iterador = 1
+edadNac = 2023 - edad
+while (iterador <= edad):
+    edadNac += 1
+    print("En el anio {}  cumpliste {} anios".format(edadNac,iterador))
+    iterador +=1
+     
+
+  
