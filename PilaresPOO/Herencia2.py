8fa39c8981f59077f1fdf28f1c530712b943f583
Rony López <49849572+RonyLopez@users.noreply.github.com>
2023-10-08 22:00:13 -0600

    pooultimo

--- PilaresPOO/Herencia2.py
+++ PilaresPOO/Herencia2.py
@@ -7,6 +7,16 @@ class Perro(Animales):
         super().__init__(nombre)
         self.sonido = sonido
 
+class Gato(Perro):
+    def __init__(self,nombre,sonido,ronronea):
+        super().__init__(nombre,sonido)
+        self.ronronea = ronronea
+
 perro = Perro("Firulais", "Guaaaaaaaooo!")
 print(perro.nombre)
 print(perro.sonido)
+
+gatito = Gato("RadioMichi","miauuuu",True)
+print(gatito.nombre)
+print(gatito.sonido)
+print(gatito.ronronea)
